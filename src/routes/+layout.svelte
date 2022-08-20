9a17837cf0941d41d12826109ae2ac6765f9c677
Don Gumaygay <dngumayagay@gmail.com>
2022-08-20 15:52:02 +0800

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.postcss';
 </script>
 
+<nav class=" bg-blue-400 flex gap-8">
+	<a class=" text-gray-700" href="/">Homepage</a>
+	<a class=" text-gray-700" href="/about">About page</a>
+</nav>
 <slot />
