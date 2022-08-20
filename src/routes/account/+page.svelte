8e1589c838712b00a186d0096680a1713aa0ac8b
Don Gumaygay <dngumayagay@gmail.com>
2022-08-20 17:53:00 +0800

    completed crud todo

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,9 +1,54 @@
 <script>
 	import { user } from '../../lib/stores';
 	import { signOut } from 'firebase/auth';
-	import { auth } from '$lib/firebaseClient';
+	import { auth, db } from '$lib/firebaseClient';
+	import {
+		collection,
+		addDoc,
+		query,
+		where,
+		onSnapshot,
+		doc,
+		deleteDoc,
+		updateDoc
+	} from 'firebase/firestore';
+
+	let listOfTodos = [];
+
+	const todoCollection = collection(db, 'todos');
+	const q = query(todoCollection, where('owner', '==', $user.uid));
+
+	const unsubscribe = onSnapshot(q, (querySnapshot) => {
+		const todos = [];
+		querySnapshot.forEach((doc) => {
+			todos.push({ ...doc.data(), id: doc.id });
+		});
+		listOfTodos = [...todos];
+		console.log(listOfTodos);
+	});
+
+	async function addTodo() {
+		const text = prompt('add todo');
+		if (text == null || text == '') return;
+
+		await addDoc(todoCollection, {
+			text: text,
+			owner: $user.uid
+		});
+	}
+
 	async function logout() {
 		await signOut(auth);
+	}
+
+	async function deleteTodo(id) {
+		await deleteDoc(doc(db, 'todos', id));
+	}
+
+	async function updateTodo(id, text) {
+		await updateDoc(doc(db, 'todos', id), {
+			text: text
+		});
 	}
 </script>
 
@@ -12,23 +57,25 @@
 		<div class="flex flex-col gap-4">
 			<h1 class="text-2xl">Hi {$user.email}</h1>
 			<button class="bg-black text-white px-3 py-2" on:click={logout}>logout</button>
-			<button class=" bg-indigo-500 py-2">add todo</button>
+			<button on:click={addTodo} class=" bg-indigo-500 py-2">add todo</button>
 			<ul class=" py-4 flex flex-col gap-4">
-				<li class="text-black">
-					<input class=" p-1" type="text" value="text" />
-					<button class="bg-green-500 text-white p-1">update</button>
-					<button class="bg-red-500 text-white p-1">delete</button>
-				</li>
-				<li class="text-black">
-					<input class=" p-1" type="text" value="text" />
-					<button class="bg-green-500 text-white p-1">update</button>
-					<button class="bg-red-500 text-white p-1">delete</button>
-				</li>
-				<li class="text-black">
-					<input class=" p-1" type="text" value="text" />
-					<button class="bg-green-500 text-white p-1">update</button>
-					<button class="bg-red-500 text-white p-1">delete</button>
-				</li>
+				{#each listOfTodos as item}
+					<li class="text-black">
+						<input class=" p-1" type="text" bind:value={item.text} />
+						<button
+							on:click={() => {
+								updateTodo(item.id, item.text);
+							}}
+							class="bg-green-500 text-white p-1">update</button
+						>
+						<button
+							on:click={() => {
+								deleteTodo(item.id);
+							}}
+							class="bg-red-500 text-white p-1">delete</button
+						>
+					</li>
+				{/each}
 			</ul>
 		</div>
 	{:else}
